f08cbad24f65a185353565d01cf5204cbc7fa352
Cuong Doan <cuong.doan@asnet.com.vn>
2025-02-21 15:33:59 +0700

    feat(students, courses, instructors): Update models.

--- django-practice/src/instructors/models.py
+++ django-practice/src/instructors/models.py
@@ -4,15 +4,46 @@ from django.conf import settings
 from core.models import AbstractBaseModel
 
 
+class Subject(AbstractBaseModel):
+    """
+    Subject model representing a subject that an instructor can specialize in.
+
+    Attributes:
+        name (CharField): The name of the subject.
+    """
+
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Instructor(AbstractBaseModel):
     """
-    Instructor model
+    Instructor model representing an instructor.
+
+    Attributes:
+        user (OneToOneField): The user associated with the instructor profile.
+        specialization (ManyToManyField): The subjects that the instructor specializes in.
+        salary (DecimalField): The salary of the instructor.
     """
 
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="instructor",
+        related_name="instructor_profile",
+        help_text="The user associated with the instructor profile.",
+    )
+    specialization = models.ManyToManyField(
+        Subject,
+        related_name="instructors",
+        help_text="The subjects that the instructor specializes in.",
+    )
+    salary = models.DecimalField(
+        max_digits=10,
+        decimal_places=3,
+        help_text="The salary of the instructor.",
+        default=0.00,
     )
 
     def __str__(self):
--- django-practice/src/students/models.py
+++ django-practice/src/students/models.py
@@ -5,14 +5,36 @@ from core.models import AbstractBaseModel
 
 class Student(AbstractBaseModel):
     """
-    Student model
+    Student model representing a student.
+
+    Attributes:
+        SCHOLARSHIP_CHOICES (list): The scholarship choices available for the student.
+        user (OneToOneField): The user associated with the student profile.
+        scholarship (CharField): The scholarship amount for the student.
     """
+
+    SCHOLARSHIP_CHOICES = [
+        (0, "0%"),
+        (25, "25%"),
+        (50, "50%"),
+        (75, "75%"),
+        (100, "100%"),
+    ]
 
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="student",
+        related_name="student_profile",
+    )
+    scholarship = models.CharField(
+        max_length=3,
+        choices=SCHOLARSHIP_CHOICES,
+        default=0,
+        help_text="The scholarship amount for the student.",
     )
 
     def __str__(self):
+        """
+        Returns the string representation of the student.
+        """
         return self.user.username
