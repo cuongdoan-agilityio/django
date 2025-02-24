76cbbb1419a4eeb4ee161ece01bc887336e337ed
Cuong Doan <cuong.doan@asnet.com.vn>
2025-02-24 14:41:40 +0700

    feat(instructors): Add subject admin.

--- learning-platform/src/instructors/admin.py
+++ learning-platform/src/instructors/admin.py
@@ -1,1 +1,20 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Subject
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    """
+    Admin interface options for the Subject model.
+
+    Displays the name of the subject in the list view and allows searching by name.
+    """
+
+    list_display = [
+        "name",
+    ]
+
+    search_fields = [
+        "name",
+    ]
