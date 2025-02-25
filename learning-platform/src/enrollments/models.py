5ef75f5b5f4fb56e6e3e3f8d789c8642ad3934e4
Cuong Doan <cuong.doan@asnet.com.vn>
2025-02-25 15:44:06 +0700

    feat(enrollments, students): Update models.

--- learning-platform/src/enrollments/models.py
+++ learning-platform/src/enrollments/models.py
@@ -15,7 +15,10 @@ class Enrollment(AbstractBaseModel):
     student = models.ForeignKey(
         "students.Student", on_delete=models.CASCADE, related_name="enrollments"
     )
-    enrolled_at = models.DateTimeField(
+    enrolled_at = models.DateField(
         default=timezone.now,
         help_text="The date and time when the student enrolled in the course.",
     )
+
+    def __str__(self):
+        return f"{self.student.user.first_name} enrolled in {self.course.title}"
