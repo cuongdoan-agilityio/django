eef2c9d5d664395f13c17c6513779a8298236825
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-03 17:29:15 +0700

    feat(core): Update config.

--- learning-platform/src/config/urls.py
+++ learning-platform/src/config/urls.py
@@ -1,10 +1,19 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.admindocs import urls as admindocs_urls
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin-dashboard/doc/", include(admindocs_urls)),
+    path("admin-dashboard/", admin.site.urls),
+    path("docs/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
 
 # API URLS
