19ffc85a8586deebfeaadf95cd653956daebc19e
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-03 17:22:43 +0700

    feat(enrollments): Update view set.

--- learning-platform/src/enrollments/api/views.py
+++ learning-platform/src/enrollments/api/views.py
@@ -1,21 +1,10 @@
-from rest_framework.permissions import AllowAny
-
 from core.api_views import BaseModelViewSet
-from ..models import Enrollment
-from .serializers import EnrollmentSerializer
 
 
 class EnrollmentViewSet(BaseModelViewSet):
     """Enrollment view set"""
 
-    resource_name = "enrollments"
-    serializer_class = EnrollmentSerializer
-    queryset = Enrollment.objects.all()
-    permission_classes = [AllowAny]
-    http_method_names = ["get"]
-
-    def get_queryset(self, *args, **kwargs):
-        return self.queryset.filter()
+    pass
 
 
 apps = [EnrollmentViewSet]
