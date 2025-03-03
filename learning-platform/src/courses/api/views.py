a83885a32db5bb5bc37860b212c6d88b810500b8
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-03 17:23:45 +0700

    feat(courses): Update view set.

--- learning-platform/src/courses/api/views.py
+++ learning-platform/src/courses/api/views.py
@@ -1,8 +1,4 @@
-from rest_framework.permissions import AllowAny
-
 from core.api_views import BaseModelViewSet
-from ..models import Course
-from .serializers import CourseSerializer
 
 
 class CourseViewSet(BaseModelViewSet):
@@ -10,14 +6,7 @@ class CourseViewSet(BaseModelViewSet):
     Course view set
     """
 
-    resource_name = "courses"
-    serializer_class = CourseSerializer
-    queryset = Course.objects.all()
-    permission_classes = [AllowAny]
-    http_method_names = ["get"]
-
-    def get_queryset(self, *args, **kwargs):
-        return self.queryset.filter()
+    pass
 
 
 apps = [CourseViewSet]
