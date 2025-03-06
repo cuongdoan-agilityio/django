e85cd606bce05a45508b0d39385263820f6a96ee
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-06 11:02:17 +0700

    feat(courses): Update courses serializers.

--- learning-platform/src/courses/api/serializers.py
+++ learning-platform/src/courses/api/serializers.py
@@ -25,31 +25,11 @@ class CourseDataSerializer(serializers.ModelSerializer):
 
 class CourseListSerializer(serializers.Serializer):
     """
-    Serializer for course data.
-
-    Fields:
-        uuid (UUIDField): The UUID of the course.
-        title (CharField): The title of the course.
-        description (CharField): The description of the course.
-        category (CharField): The category of the course.
-        instructor (CharField): The name of the instructor.
-        status (CharField): The status of the course.
+    Serializer for a list of courses with pagination metadata.
     """
 
     data = CourseDataSerializer(many=True)
     meta = MetaSerializer()
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        meta = {
-            "pagination": {
-                "total": instance.count(),
-                "limit": self.paginator.page_size,
-                "page": self.paginator.page.start_index(),
-            }
-        }
-
-        return {"data": data, "meta": MetaSerializer(meta).data}
 
 
 class CourseSerializer(serializers.Serializer):
