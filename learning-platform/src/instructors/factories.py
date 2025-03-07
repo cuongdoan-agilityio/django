1c3fb7ec183031274537fc144acc20f05451ed42
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-07 17:11:40 +0700

    feat(instructors): Unit test for Instructor and Subjects models.

---
+++ learning-platform/src/instructors/factories.py
@@ -0,0 +1,52 @@
+from django.contrib.auth import get_user_model
+
+from factory.django import DjangoModelFactory as ModelFactory
+from factory import Sequence, post_generation
+from factory import SubFactory, Iterator
+
+from accounts.factories import UserFactory
+from instructors.models import Instructor, Subject
+from core.constants import Degree
+
+
+User = get_user_model()
+
+
+class SubjectFactory(ModelFactory):
+    """
+    Faker for Subject model.
+    """
+
+    name = Sequence(lambda n: f"Subject_{n}")
+
+    class Meta:
+        """
+        Meta class
+        """
+
+        model = Subject
+
+
+class InstructorFactory(ModelFactory):
+    """
+    Faker for Instructor model.
+    """
+
+    user = SubFactory(UserFactory)
+    degree = Iterator(Degree.values())
+
+    @post_generation
+    def subjects(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for subject in extracted:
+                self.subjects.add(subject)
+
+    class Meta:
+        """
+        Meta class
+        """
+
+        model = Instructor
