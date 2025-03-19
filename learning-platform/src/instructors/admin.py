5141373800fc3cd914d28a667d2a4eb7c12f9d93
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-19 17:11:53 +0700

    refactor(students, instructors): Update inline forms.

--- learning-platform/src/instructors/admin.py
+++ learning-platform/src/instructors/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from core.filters import GenderFilter
 
 from .models import Instructor, Subject
-from .forms import InstructorBaseForm, InstructorEditForm, CourseInlineFormSet
+from .forms import InstructorBaseForm, InstructorEditForm
 from courses.models import Course
 
 
@@ -46,7 +46,7 @@ class CourseInline(admin.TabularInline):
     extra = 0
     fields = ["title", "description", "category", "status", "image_url"]
     readonly_fields = ["instructor"]
-    formset = CourseInlineFormSet
+    autocomplete_fields = ["category"]
 
     def get_queryset(self, request):
         return super().get_queryset(request).select_related("category")
