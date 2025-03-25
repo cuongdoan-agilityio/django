7925bcb01a4c46088a5a5e8e0d23fbb6b60892f8
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-25 17:19:17 +0700

    Add split models.

---
+++ superbook/posts/models/post.py
@@ -0,0 +1,28 @@
+from django.db import models
+from models.superhero import SuperHero
+from models.mixin import TimeStampedMixin
+
+
+class ActivePostManager(models.Manager):
+    """
+    ActivePostManager
+    """
+
+    def get_queryset(self):
+        return super().get_queryset().filter(content__icontains="active")
+
+
+class Post(TimeStampedMixin):
+    """
+    Post model.
+    """
+
+    hero = models.ForeignKey(SuperHero, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    likes = models.IntegerField(default=0)
+
+    objects = models.Manager()
+    active_objects = ActivePostManager()
+
+    def __str__(self):
+        return f"{self.hero.name}: {self.content}"
---
+++ superbook/posts/models/superhero.py
@@ -0,0 +1,14 @@
+from django.db import models
+from models.mixin import TimeStampedMixin
+
+
+class SuperHero(TimeStampedMixin):
+    """
+    SuperHero model
+    """
+
+    name = models.CharField(max_length=100)
+    power = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
