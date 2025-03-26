8fb957ddbebe6e21f451fee9302d39c0e4015fb6
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-26 18:05:22 +0700

    Add accounts admin.

--- superbook/accounts/admin.py
+++ superbook/accounts/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+from django.conf import settings
+
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Admin class for Profile model.
+    """
+
+    list_display = ("user", "hero_name", "power")
+    list_per_page = settings.LIST_PER_PAGE
