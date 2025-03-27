a50f9360256b422192a7737adea4d25db7582ed1
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-27 17:56:48 +0700

    Update urls.

--- superbook/posts/urls.py
+++ superbook/posts/urls.py
@@ -6,4 +6,6 @@ app_name = "posts"
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="index"),
+    path("adds", views.PostCreateView.as_view(), name="adds"),
+    path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
 ]
