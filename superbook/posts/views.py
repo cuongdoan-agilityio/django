26a1dbe86282c1b7534d7531f3370faa5b44ac0b
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-27 17:54:29 +0700

    Add PostCreateView and PostDetailView.

--- superbook/posts/views.py
+++ superbook/posts/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, FormView, UpdateView
 from posts.models.post import Post
+from posts.forms import PostForm
 
 
 class PostListView(ListView):
@@ -13,3 +14,31 @@ class PostListView(ListView):
 
     def get_queryset(self):
         return Post.objects.all().order_by("-updated_at")
+
+
+class PostCreateView(FormView):
+    """
+    Post Create View
+    """
+
+    template_name = "posts/detail.html"
+    form_class = PostForm
+    context_object_name = "post"
+    success_url = "/posts/"
+    model = Post
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class PostDetailView(UpdateView):
+    """
+    Post Detail View
+    """
+
+    template_name = "posts/detail.html"
+    form_class = PostForm
+    model = Post
+    context_object_name = "post"
+    success_url = "/posts/"
