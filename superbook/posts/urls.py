7fdd596ea2d4a2763005d3a8d54404d1ff8fa656
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-28 17:20:14 +0700

    Add API get posts.

--- superbook/posts/urls.py
+++ superbook/posts/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
-    path("", views.PostListView.as_view(), name="index"),
+    path("list", views.PostListView.as_view(), name="index"),
     path("adds", views.PostCreateView.as_view(), name="adds"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
+    path("", views.PostView.as_view(), name="post-list"),
 ]
--- superbook/posts/views.py
+++ superbook/posts/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import ListView, FormView, UpdateView
+from rest_framework import generics
+from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
+from rest_framework.response import Response
+
 from posts.models.post import Post
 from posts.forms import PostForm
+from posts.serializers import PostSerializer
+from posts.pagination import PostPagination
 
 
 class PostListView(ListView):
@@ -14,6 +20,30 @@ class PostListView(ListView):
 
     def get_queryset(self):
         return Post.objects.all().order_by("-updated_at")
+
+
+class PostView(generics.ListCreateAPIView):
+    """
+    PostListView
+    """
+
+    renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
+    serializer_class = PostSerializer
+    pagination_class = PostPagination
+
+    def get_queryset(self):
+        return Post.objects.all().order_by("-updated_at")
+
+    def get(self, request, *args, **kwargs):
+        post_list = self.get_queryset()
+        paginator = self.pagination_class()
+        result_page = paginator.paginate_queryset(post_list, request)
+
+        if request.accepted_renderer.format == "html":
+            return Response({"post_list": result_page}, template_name="posts/list.html")
+
+        serializer = PostSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
 
 class PostCreateView(FormView):
