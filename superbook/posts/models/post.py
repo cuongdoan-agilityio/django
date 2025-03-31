c16360db022b73699f400d6332d8ac9e5a31db3d
Cuong Doan <cuong.doan@asnet.com.vn>
2025-03-31 18:19:27 +0700

    Update post model.

--- superbook/posts/models/post.py
+++ superbook/posts/models/post.py
@@ -20,6 +20,7 @@ class Post(TimeStampedMixin):
     hero = models.ForeignKey(SuperHero, on_delete=models.CASCADE, related_name="posts")
     content = models.TextField()
     likes = models.IntegerField(default=0)
+    title = models.CharField(max_length=255, null=True, blank=True)
 
     objects = models.Manager()
     active_objects = ActivePostManager()
