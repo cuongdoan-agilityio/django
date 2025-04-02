b2deb771d24f97b08887c7826953969c6322562e
Cuong Doan <cuong.doan@asnet.com.vn>
2025-04-02 18:02:14 +0700

    Update sent_notification task.

--- superbook/posts/tasks.py
+++ superbook/posts/tasks.py
@@ -1,12 +1,11 @@
 from superbook.celery import app
 
 
-@app.task(name="post.create_post")
+@app.task(name="post.sent_notification")
 def sent_notification() -> float:
     """
     Sent notification
     TODO:: Need update code to sent the email.
     """
-
-    print("Sent notification")
-    return
+    message = "Sent notification"
+    return message
