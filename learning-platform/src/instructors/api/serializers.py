2927650cbd432aaed10d7f0cfbbb7de8c27e0f93
Cuong Doan <cuong.doan@asnet.com.vn>
2025-04-23 11:36:47 +0700

    Remove instructor profile serializer.

--- learning-platform/src/instructors/api/serializers.py
+++ learning-platform/src/instructors/api/serializers.py
@@ -9,44 +9,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class InstructorProfileDataSerializer(serializers.ModelSerializer):
-    """
-    Serializer for instructor profiles data.
-    """
-
-    subjects = serializers.SerializerMethodField()
-    degree = serializers.SerializerMethodField()
-
-    class Meta:
-        model = User
-        fields = [
-            "id",
-            "username",
-            "first_name",
-            "last_name",
-            "email",
-            "phone_number",
-            "date_of_birth",
-            "gender",
-            "degree",
-            "subjects",
-        ]
-
-    def get_degree(self, obj) -> str | None:
-        """
-        Retrieves the instructor degree.
-        """
-
-        return obj.instructor_profile.degree
-
-    def get_subjects(self, obj) -> list[str] | None:
-        """
-        Retrieves the instructor subjects.
-        """
-
-        return [subject.id for subject in obj.instructor_profile.subjects.all()]
-
-
 class InstructorBaseSerializer(serializers.ModelSerializer):
     """
     Serializer for Instructor serializer.
