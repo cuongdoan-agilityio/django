7250de7424bd38fd47f86b50985036107e5440ae
Cuong Doan <cuong.doan@asnet.com.vn>
2025-04-25 11:02:54 +0700

    Init notification admin page.

--- learning-platform/src/notifications/admin.py
+++ learning-platform/src/notifications/admin.py
@@ -1,1 +1,19 @@
-# Register your models here.
+from django.contrib import admin
+from django.conf import settings
+from notifications.models import Notification
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Notification model.
+    """
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("user")
+
+    list_display = ("id", "user", "message", "is_read", "modified")
+    list_per_page = settings.ADMIN_PAGE_SIZE
+    list_filter = ["is_read", "user"]
+    search_fields = ["message"]
+    ordering = ["-modified"]
