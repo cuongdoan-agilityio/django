464a5ce329a75ddcca562955303bf3cac39d6540
Cuong Doan <cuong.doan@asnet.com.vn>
2025-05-08 18:32:58 +0700

    Implement send_monthly_report task.

--- learning-platform/src/courses/tasks.py
+++ learning-platform/src/courses/tasks.py
@@ -1,4 +1,13 @@
 from celery import shared_task
+import csv
+from io import StringIO
+import base64
+from core.constants import Role
+from django.contrib.auth import get_user_model
+from core.helpers import send_report_email
+
+
+User = get_user_model()
 
 
 @shared_task
@@ -17,4 +26,19 @@ def send_monthly_report():
     about statistical number of enrolled students per their course.
     """
 
-    return
+    instructors = User.objects.filter(role=Role.INSTRUCTOR.value)
+
+    for instructor in instructors:
+        courses = instructor.courses.all()
+        csv_file = StringIO()
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["Course Title", "Number of Enrolled"])
+        for course in courses:
+            student_count = course.enrollments.count()
+
+            csv_writer.writerow([course.title, student_count])
+
+        csv_file.seek(0)
+        encoded_csv = base64.b64encode(csv_file.getvalue().encode()).decode()
+
+        send_report_email(instructor=instructor, csv_file=encoded_csv)
