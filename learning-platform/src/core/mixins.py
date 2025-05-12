e3bb79c55c510b93bab7ea50ce980c8b547ec2fc
Cuong Doan <cuong.doan@asnet.com.vn>
2025-05-12 14:07:16 +0700

    Update format_data method.

--- learning-platform/src/core/mixins.py
+++ learning-platform/src/core/mixins.py
@@ -5,6 +5,7 @@ class FormatDataMixin:
 
     def format_data(self, instance, *args, **kwargs):
         serializer_class = self.get_serializer_class()
+        kwargs["context"] = self.get_serializer_context()
         serializer = serializer_class(instance, *args, **kwargs)
         data = serializer.data
         return {"data": {**data}}
