57676024fe70714220ad94cf5f3918200f514b5c
Cuong Doan <cuong.doan@asnet.com.vn>
2025-05-15 11:19:42 +0700

    Update course signal unit test.

--- learning-platform/src/courses/tests/conftest.py
+++ learning-platform/src/courses/tests/conftest.py
@@ -5,9 +5,11 @@ from django.db.models.signals import post_save
 from accounts.signals import send_verify_email
 from django.contrib.auth import get_user_model
 from accounts.factories import UserFactory
-from core.constants import Role
-from core.constants import Status
+from core.constants import Role, Status
 from courses.factories import EnrollmentFactory
+from courses.models import Enrollment
+from courses.signals import send_email_to_instructor
+
 
 User = get_user_model()
 
@@ -15,14 +17,27 @@ User = get_user_model()
 @pytest.fixture
 def faker():
     """
-    Fixture to provide an isolated Faker instance.
+    Fixture to provide an isolated Faker instance for generating fake data.
     """
 
     return Faker()
 
 
 @pytest.fixture
+def disconnect_signal():
+    """
+    Fixture to disconnect the signal.
+    """
+
+    post_save.disconnect(receiver=send_verify_email, sender=User)
+
+
+@pytest.fixture
 def share_data(faker):
+    """
+    Fixture to provide shared sample data for testing.
+    """
+
     return {
         "password": "Password@123",
         "username": faker.user_name(),
@@ -36,19 +51,23 @@ def share_data(faker):
 
 @pytest.fixture
 def course_data(faker):
-    """Fixture to provide sample course data."""
+    """
+    Fixture to provide sample course data.
+    Includes fields like title and description.
+    """
+
     title = faker.sentence(nb_words=6)
     description = faker.paragraph(nb_sentences=3)
     return {"title": title, "description": description}
 
 
 @pytest.fixture
-def fake_course(course_data, db):
+def fake_course(course_data, disconnect_signal, db):
     """
     Fixture to create a sample Course instance.
+    Disconnects the `send_verify_email` signal to avoid side effects.
     """
 
-    post_save.disconnect(receiver=send_verify_email, sender=User)
     return CourseFactory(
         title=course_data["title"],
         description=course_data["description"],
@@ -60,6 +79,7 @@ def fake_course(course_data, db):
 def category_data(faker):
     """
     Fixture to provide sample category data.
+    Includes fields like name and description.
     """
 
     return {
@@ -71,14 +91,19 @@ def category_data(faker):
 @pytest.fixture
 def fake_category(category_data):
     """
-    Fixture to create a Category instance.
+    Fixture to create a Category instance using the provided category data.
     """
+
     return CategoryFactory(**category_data)
 
 
 @pytest.fixture
-def fake_student():
-    post_save.disconnect(receiver=send_verify_email, sender=User)
+def fake_student(disconnect_signal):
+    """
+    Fixture to create a sample student user.
+    Disconnects the `send_verify_email` signal to avoid side effects.
+    """
+
     return UserFactory(
         password="Password@123",
         role=Role.STUDENT.value,
@@ -87,28 +112,43 @@ def fake_student():
 
 @pytest.fixture
 def fake_enrollment(fake_course, fake_student):
-    """Fixture tạo một Enrollment với course và student_user."""
+    """
+    Fixture to create an Enrollment instance with a course and student.
+    """
+
     return EnrollmentFactory(course=fake_course, student=fake_student)
 
 
 @pytest.fixture
-def fake_instructor():
-    """Fixture to create an instructor user."""
-    post_save.disconnect(receiver=send_verify_email, sender=User)
+def fake_instructor(disconnect_signal):
+    """
+    Fixture to create a sample instructor user.
+    Disconnects the `send_verify_email` signal to avoid side effects.
+    """
+
     return UserFactory(role=Role.INSTRUCTOR.value)
 
 
 @pytest.fixture
 def math_course(fake_instructor):
+    """
+    Fixture to create a sample Math course with an instructor.
+    """
+
     return CourseFactory(
         title="Math course",
         status=Status.ACTIVATE.value,
+        enrollment_limit=2,
         instructor=fake_instructor,
     )
 
 
 @pytest.fixture
 def music_course(fake_instructor):
+    """
+    Fixture to create a sample Music course with an instructor.
+    """
+
     return CourseFactory(
         title="Music Course",
         status=Status.ACTIVATE.value,
@@ -117,21 +157,48 @@ def music_course(fake_instructor):
 
 
 @pytest.fixture
-def other_student_user():
-    post_save.disconnect(receiver=send_verify_email, sender=User)
+def other_student_user(disconnect_signal):
+    """
+    Fixture to create another sample student user.
+    Disconnects the `send_verify_email` signal to avoid side effects.
+    """
+
     return UserFactory(role=Role.STUDENT.value)
 
 
 @pytest.fixture
 def math_enrollment(math_course, fake_student):
+    """
+    Fixture to create an Enrollment instance for the Math course and a student.
+    """
+
     return EnrollmentFactory(course=math_course, student=fake_student)
 
 
 @pytest.fixture
 def math_enrollment_other(math_course, other_student_user):
+    """
+    Fixture to fake math enrollment for other user.
+    """
+
     return EnrollmentFactory(course=math_course, student=other_student_user)
 
 
 @pytest.fixture
 def music_enrollment(music_course, fake_student):
+    """
+    Fixture to create an Enrollment instance for the Music course and a student.
+    """
+
     return EnrollmentFactory(course=music_course, student=fake_student)
+
+
+@pytest.fixture
+def connect_signal():
+    """
+    Fixture to connect the signal, and disconnect after the test
+    """
+
+    post_save.connect(receiver=send_email_to_instructor, sender=Enrollment)
+    yield
+    post_save.disconnect(receiver=send_email_to_instructor, sender=Enrollment)
