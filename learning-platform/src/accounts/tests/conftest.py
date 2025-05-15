374ca7caec01e8ee0aba2e17f1005f63587cea06
Cuong Doan <cuong.doan@asnet.com.vn>
2025-05-15 14:16:21 +0700

    Update accounts signal unit test.

--- learning-platform/src/accounts/tests/conftest.py
+++ learning-platform/src/accounts/tests/conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from faker import Faker
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 
 from accounts.factories import SpecializationFactory
+from accounts.signals import send_verify_email, enroll_intro_course
 
 
 fake = Faker()
+User = get_user_model()
 
 
 @pytest.fixture
@@ -27,3 +31,25 @@ def fake_specialization(specialization_data):
     return SpecializationFactory(
         **specialization_data
     )
+
+
+@pytest.fixture
+def user_data():
+    username = fake.user_name()
+    email = fake.email()
+    password = "Password@123"
+    return {"username": username, "email": email, "password": password}
+
+
+@pytest.fixture
+def send_verify_email_signal():
+    post_save.connect(receiver=send_verify_email, sender=User)
+    yield
+    post_save.disconnect(receiver=send_verify_email, sender=User)
+
+
+@pytest.fixture
+def enroll_intro_course_signal():
+    post_save.connect(receiver=enroll_intro_course, sender=User)
+    yield
+    post_save.disconnect(receiver=enroll_intro_course, sender=User)
