8857bf4fa283374b7dd400edf47eef578f5252c4
Cuong Doan <cuong.doan@asnet.com.vn>
2025-05-24 22:30:08 +0700

    Unit test for EnrollmentInlineForm.

--- learning-platform/src/courses/tests/test_enrollment_forms.py
+++ learning-platform/src/courses/tests/test_enrollment_forms.py
@@ -1,5 +1,7 @@
 import pytest
-from courses.forms import EnrollmentForm
+from django.core.exceptions import ValidationError
+from courses.forms import EnrollmentForm, EnrollmentInlineForm
+from courses.factories import EnrollmentFactory
 from core.constants import Status
 from core.error_messages import ErrorMessage
 
@@ -78,3 +80,66 @@ class TestEnrollmentForm:
         form = EnrollmentForm(data=form_data)
 
         assert not form.is_valid()
+
+
+@pytest.mark.django_db
+class TestEnrollmentInlineForm:
+    """
+    Test suite for the EnrollmentInlineForm.
+    """
+
+    def test_valid_enrollment(self, fake_course, fake_student):
+        """
+        Test that the form is valid when the course is active, the student is valid, and the student is not already enrolled.
+        """
+
+        form = EnrollmentInlineForm(
+            data={"student": fake_student.id},
+            instance=EnrollmentFactory(course=fake_course),
+        )
+        assert form.is_valid()
+
+    def test_inactive_course(self, fake_course, fake_student):
+        """
+        Test that the form raises an error when the course is inactive.
+        """
+
+        fake_course.status = Status.INACTIVE.value
+        fake_course.save()
+
+        with pytest.raises(ValidationError) as err:
+            EnrollmentInlineForm(
+                data={"student": fake_student.id},
+                instance=EnrollmentFactory(course=fake_course),
+            )
+
+        assert ErrorMessage.INACTIVE_COURSE in str(err.value)
+
+    def test_student_already_enrolled(self, fake_course, fake_student):
+        """
+        Test that the form raises an error when the student is already enrolled in the course.
+        """
+
+        EnrollmentFactory(course=fake_course, student=fake_student)
+        enrollment = EnrollmentFactory(course=fake_course)
+        form = EnrollmentInlineForm(
+            data={"student": fake_student.id}, instance=enrollment
+        )
+
+        with pytest.raises(ValidationError) as err:
+            form.is_valid()
+            form.clean()
+
+        assert ErrorMessage.STUDENT_ALREADY_ENROLLED in str(err.value)
+
+    def test_no_changes(self, fake_course, fake_student):
+        """
+        Test that the form does not raise validation errors if no changes are made.
+        """
+
+        enrollment = EnrollmentFactory(course=fake_course, student=fake_student)
+        form = EnrollmentInlineForm(
+            data={"student": fake_student.id}, instance=enrollment
+        )
+
+        assert form.is_valid()
