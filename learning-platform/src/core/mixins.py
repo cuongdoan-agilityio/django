152d47980404eb90e757b3e30903dda6f47c1552
Cuong Doan <cuong.doan@asnet.com.vn>
2025-06-02 16:13:17 +0700

    Define CustomListModelMixin.

--- learning-platform/src/core/mixins.py
+++ learning-platform/src/core/mixins.py
@@ -1,3 +1,6 @@
+from rest_framework.response import Response
+
+
 class FormatDataMixin:
     """
     Format the response data.
@@ -21,3 +24,33 @@ class FormatDataMixin:
         serializer = serializer_class(data, many=True, *args, **kwargs)
         data = serializer.data
         return {"data": data}
+
+
+class CustomListModelMixin:
+    """
+    List a queryset.
+    """
+
+    def list(self, request, *args, **kwargs):
+        """
+        List a queryset with optional pagination.
+
+        This method retrieves a filtered queryset, applies pagination if necessary,
+        and formats the response data to include metadata for paginated results.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            pagination_data = self.get_paginated_response(page)
+            data = pagination_data.data
+            serializer = self.get_serializer(
+                {
+                    "data": data.get("data"),
+                    "meta": data.get("meta"),
+                }
+            )
+            return Response(serializer.data)
+
+        serializer = self.get_serializer({"data": queryset})
+        return Response(serializer.data)
