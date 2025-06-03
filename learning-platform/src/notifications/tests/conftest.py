68630054cdf3cada0be7b0cc4b4eb1099187cf0b
Cuong Doan <cuong.doan@asnet.com.vn>
2025-06-03 10:33:06 +0700

    Remove unused fixture.

--- learning-platform/src/notifications/tests/conftest.py
+++ learning-platform/src/notifications/tests/conftest.py
@@ -43,34 +43,3 @@ def fake_notification(fake_user, share_data):
         message=share_data["message"],
         is_read=share_data["is_read"],
     )
-
-
-@pytest.fixture
-def notification_url(root_url):
-    """
-    Fixture to provide the base URL for notifications list.
-    """
-    return f"{root_url}notifications/"
-
-
-@pytest.fixture
-def fake_student_notifications(fake_student):
-    """
-    Fixture to create notifications for the student user.
-    """
-    return [
-        NotificationFactory(user=fake_student, is_read=False),
-        NotificationFactory(user=fake_student, is_read=True),
-    ]
-
-
-@pytest.fixture
-def fake_instructor_notifications(fake_instructor):
-    """
-    Fixture to create notifications for the instructor user.
-    """
-
-    return [
-        NotificationFactory(user=fake_instructor, is_read=False),
-        NotificationFactory(user=fake_instructor, is_read=True),
-    ]
