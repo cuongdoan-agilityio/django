8019a178078634aa341ba519e97f82710e8efda6
Cuong Doan <cuong.doan@asnet.com.vn>
2025-06-03 14:56:28 +0700

    Update course task unit test.

--- learning-platform/src/courses/tests/test_task.py
+++ learning-platform/src/courses/tests/test_task.py
@@ -12,64 +12,66 @@ from courses.tasks import (
     report_to_instructor,
 )
 from courses.factories import CourseFactory
+from .base import BaseCourseModuleTestCase
 
 
 User = get_user_model()
 
 
-@pytest.mark.django_db
-class TestCleanUpInactiveCoursesTask:
+class TestCleanUpInactiveCoursesTask(BaseCourseModuleTestCase):
     """
     Unit tests for the clean_up_inactive_courses Celery task.
     """
 
-    def test_clean_up_inactive_courses_success(self, fake_instructor):
+    def test_clean_up_inactive_courses_success(self):
         """
         Test that inactive courses older than 3 months are deleted.
         """
 
-        old_inactive_course = self.fake_course(
-            datetime.now() - relativedelta(months=4), instructor=fake_instructor
+        old_inactive_course = self.fake_course_instance(
+            datetime.now() - relativedelta(months=4), instructor=self.fake_instructor
         )
         clean_up_inactive_courses()
         assert not Course.objects.filter(id=old_inactive_course.id).exists()
 
-    def test_active_courses_not_deleted(self, fake_instructor):
+    def test_active_courses_not_deleted(self):
         """
         Test that active courses are not deleted.
         """
 
-        active_course = self.fake_course(
+        active_course = self.fake_course_instance(
             datetime.now() - relativedelta(months=2),
             Status.ACTIVATE.value,
-            instructor=fake_instructor,
+            instructor=self.fake_instructor,
         )
         clean_up_inactive_courses()
         assert Course.objects.filter(id=active_course.id).exists()
 
-    def test_recent_inactive_courses_not_deleted(self, fake_instructor):
+    def test_recent_inactive_courses_not_deleted(self):
         """
         Test that inactive courses less than 3 months old are not deleted.
         """
 
-        recent_inactive_course = self.fake_course(
-            datetime.now() - relativedelta(months=2), instructor=fake_instructor
+        recent_inactive_course = self.fake_course_instance(
+            datetime.now() - relativedelta(months=2), instructor=self.fake_instructor
         )
         clean_up_inactive_courses()
         assert Course.objects.filter(id=recent_inactive_course.id).exists()
 
-    def test_system_courses_not_deleted(self, fake_instructor):
+    def test_system_courses_not_deleted(self):
         """
         Test that system courses are not deleted.
         """
 
-        system_course = self.fake_course(
+        system_course = self.fake_course_instance(
             datetime.now() - relativedelta(months=4), Status.INACTIVE.value
         )
         clean_up_inactive_courses()
         assert Course.objects.filter(id=system_course.id).exists()
 
-    def fake_course(self, delta_time, status=Status.INACTIVE.value, instructor=None):
+    def fake_course_instance(
+        self, delta_time, status=Status.INACTIVE.value, instructor=None
+    ):
         """
         Create a fake course.
         """
@@ -86,23 +88,14 @@ class TestCleanUpInactiveCoursesTask:
         return course
 
 
-@pytest.mark.django_db
-class TestSendMonthlyReportTask:
+class TestSendMonthlyReportTask(BaseCourseModuleTestCase):
     """
     Unit tests for the send_monthly_report Celery task.
     """
 
     @patch("courses.tasks.report_to_instructor.s")
     @patch("courses.tasks.group")
-    def test_send_monthly_report_success(
-        self,
-        mock_group,
-        mock_report_to_instructor,
-        fake_instructor,
-        math_enrollment,
-        math_enrollment_other,
-        music_enrollment,
-    ):
+    def test_send_monthly_report_success(self, mock_group, mock_report_to_instructor):
         """
         Test that the monthly report is sent successfully to instructors.
         """
@@ -112,19 +105,6 @@ class TestSendMonthlyReportTask:
         send_monthly_report()
         assert mock_report_to_instructor.called
         assert mock_report_to_instructor.call_count == 1
-
-    def test_no_courses_for_instructor(self):
-        """
-        Test that the task handles instructors with no courses gracefully.
-        """
-
-        Course.objects.all().delete()
-
-        with patch(
-            "courses.tasks.send_report_email", return_value=None
-        ) as mock_send_report_email:
-            send_monthly_report()
-            mock_send_report_email.assert_not_called()
 
     def test_no_instructors_in_system(self):
         """
@@ -140,44 +120,40 @@ class TestSendMonthlyReportTask:
             mock_send_report_email.assert_not_called()
 
 
-@pytest.mark.django_db
-class TestReportToInstructorTask:
+class TestReportToInstructorTask(BaseCourseModuleTestCase):
     """
     Unit tests for the report_to_instructor Celery task.
     """
 
     @patch("courses.tasks.send_report_email")
-    def test_report_to_instructor_success(
-        self, mock_send_report_email, fake_instructor, math_enrollment, music_enrollment
-    ):
+    def test_report_to_instructor_success(self, mock_send_report_email):
         """
         Test that the report_to_instructor task generates the CSV file and sends the email successfully.
         """
 
-        report_to_instructor(fake_instructor.id)
+        report_to_instructor(self.fake_instructor.id)
         mock_send_report_email.assert_called_once()
 
         _, kwargs = mock_send_report_email.call_args
-        assert kwargs["instructor"] == fake_instructor
+        assert kwargs["instructor"] == self.fake_instructor
         assert "csv_file" in kwargs
 
     @patch("courses.tasks.send_report_email")
     def test_report_to_instructor_no_courses(
         self,
         mock_send_report_email,
-        fake_instructor,
     ):
         """
         Test that the report_to_instructor task handles no courses gracefully.
         """
 
-        report_to_instructor(fake_instructor.id)
+        Course.objects.all().delete()
+
+        report_to_instructor(self.fake_instructor.id)
         mock_send_report_email.assert_not_called()
 
     @patch("courses.tasks.send_report_email")
-    def test_report_to_instructor_email_failure(
-        self, mock_send_report_email, fake_instructor, math_enrollment, music_enrollment
-    ):
+    def test_report_to_instructor_email_failure(self, mock_send_report_email):
         """
         Test that the report_to_instructor task handles email sending failure gracefully.
         """
@@ -185,6 +161,6 @@ class TestReportToInstructorTask:
         mock_send_report_email.side_effect = Exception("Email sending failed")
 
         with pytest.raises(Exception, match="Email sending failed"):
-            report_to_instructor(fake_instructor.id)
+            report_to_instructor(self.fake_instructor.id)
 
         mock_send_report_email.assert_called_once()
